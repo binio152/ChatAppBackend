9a40f2ec49dcd1601858a543164c544b2c711bd3
Tran Xuan Binh <binidafs2k1@gmail.com>
2026-06-11 00:04:03 +0700

    refactor(profile): imporve user profile response format

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,3 +1,10 @@
 export const getProfile = async (req, res) => {
-  res.send("<h1>Profile</h1>");
+  try {
+    const user = req.user;
+
+    return res.status(200).json({ user });
+  } catch (err) {
+    console.log("Error occurred while getting user profile", err);
+    return res.status(500).json({ message: "Internal server error" });
+  }
 };
